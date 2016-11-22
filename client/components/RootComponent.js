cbc22259230f7c58d2a71069ee562be7ca80ef90
lschpmn <luis@lmc-productions.com>
2016-11-21 19:37:50 -0600

    Added react

---
+++ client/components/RootComponent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React, {Component} from 'react';
+
+export default class RootComponent extends Component {
+  render() {
+    return <div>Sup</div>;
+  }
+}
--- client/index.js
+++ client/index.js
@@ -1,3 +1,9 @@
 'use strict';
 
 import './index.html';
+
+import React from 'react';
+import {render} from 'react-dom';
+import RootComponent from './components/RootComponent';
+
+render(<RootComponent />, document.getElementById('react'));
