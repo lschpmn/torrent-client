85e2d8862d7bf04d4db353d913f5e83ef1663dea
lschpmn <luis@lmc-productions.com>
2016-11-22 21:06:30 -0600

    Changed order of imports

--- client/index.js
+++ client/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-import './index.html';
-import './style/bootstrap.min.css';
-
 import React from 'react';
 import {render} from 'react-dom';
 import RootComponent from './components/RootComponent';
+
+//website
+import './index.html';
+import './style/bootstrap.min.css';
 
 render(<RootComponent />, document.getElementById('react'));
