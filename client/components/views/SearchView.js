36d9ea762828900357a48cbda0b8cb224099c1f4
lschpmn <luis@lmc-productions.com>
2016-11-25 15:51:01 -0600

    Added SearchView

---
+++ client/components/views/SearchView.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React, {Component} from 'react';
+
+export default class SearchView extends Component {
+  render() {
+    return <div className="row">
+      <div className="col s12"><h1>Search</h1></div>
+    </div>;
+  }
+}
--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import RootComponent from './components/RootComponent';
+import SearchView from './components/views/SearchView';
 import TorrentView from './components/views/TorrentView';
 import {browserHistory, IndexRoute, Router, Route} from 'react-router';
 
@@ -15,6 +16,7 @@ render(
   <Router history={browserHistory}>
     <Route path="/" component={RootComponent}>
       <IndexRoute component={TorrentView} />
+      <Route path="/search" component={SearchView} />
     </Route>
   </Router>
   , document.getElementById('react'));
