1c6377f31f18065a5bf9f3fde1c0378c88a32a85
lschpmn <luis@lmc-productions.com>
2016-11-27 14:04:10 -0600

    Added search bar

--- client/components/views/SearchView.js
+++ client/components/views/SearchView.js
@@ -5,7 +5,21 @@ import React, {Component} from 'react';
 export default class SearchView extends Component {
   render() {
     return <div className="row">
-      <div className="col s12"><h1>Search</h1></div>
+      <nav>
+        <div className="nav-wrapper blue lighten-1">
+          <form onSubmit={e => e.preventDefault()}>
+            <div className="input-field">
+              <input id="search" type="search" />
+              <label htmlFor="search"><i className="material-icons">search</i></label>
+              <i className="material-icons">close</i>
+            </div>
+          </form>
+        </div>
+      </nav>
+      
+      <div className="col s12">
+        
+      </div>
     </div>;
   }
 }
