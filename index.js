0fe3182ae3eb7dd345160ca49c4163561782fd30
lschpmn <luis@lmc-productions.com>
2018-08-20 23:54:15 -0500

    Added start command, changed starting window size

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { app, BrowserWindow } = require('electron');
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({ width: 1280, height: 720 });
 
   win.loadURL('http://localhost:5000');
 
