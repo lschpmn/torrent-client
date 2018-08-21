21af82293c81a4fd9a03d765e3139fe2ad80a267
lschpmn <luis@lmc-productions.com>
2018-08-21 00:07:52 -0500

    Installed materia-ui

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default class App extends React.Component {
   render() {
     return <div>
       <h1>Hello World</h1>
+      <Button>Click me!!</Button>
     </div>;
   }
 }
