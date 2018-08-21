9898490ca62694ef72bd66fd22e8906918694521
lschpmn <luis@lmc-productions.com>
2018-08-20 23:50:40 -0500

    Starting from scratch with typescript

---
+++ client/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import './index.html';
+
+render(
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+  , document.getElementById('react'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
 'use strict';
 
-const path = require('path');
+const { join } = require('path');
 
 module.exports = {
-  context: path.join(__dirname, 'client'),
-  entry: './index.js',
+  context: join(__dirname, 'client'),
+
+  entry: './index.tsx',
+
   output: {
-    path: path.join(__dirname, 'public'),
+    path: join(__dirname, 'public'),
     filename: 'bundle.js'
   },
-  
+
+  mode: 'development',
+
   module: {
-    loaders: [
-      {test: /\.css$/, loader: 'style!css'},
-      {test: /\.html$|\.eot$|\.woff$|\.woff2$|\.ttf$/, loader: 'file?name=[name].[ext]'},
-      {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel?presets[]=react,presets[]=es2015"}
-    ]
+    rules: [
+      { test: /.html$/, loader: 'file-loader', },
+
+      { test: /.tsx?$/, loader: 'ts-loader', },
+    ],
   },
   
-  devServer: {
-    inline: true,
+  serve: {
     port: 5000,
-    hot: true,
-    historyApiFallback: true
   }
 };
