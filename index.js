8243806ae416c3851433600b5cb058a761051342
lschpmn <luis@lmc-productions.com>
2018-08-20 23:36:22 -0500

    Simplified electron file

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 'use strict';
 
-const {app, BrowserWindow} = require('electron');
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow } = require('electron');
 
 let win;
 
-function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600});
-  
-  /*win.loadURL(url.format({
-    pathname: path.join(__dirname, './public/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));*/
-  
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+
   win.loadURL('http://localhost:5000');
-  
+
   win.webContents.openDevTools();
-  
+
   win.on('closed', () => {
-    win = null
+    process.exit();
   });
 }
 
 app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  app.quit();
-});
