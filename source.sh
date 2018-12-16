030de1d5cf1d7d3e86889def037168117eb33427
lschpmn <luis@lmc-productions.com>
2018-12-15 23:27:51 -0600

    Adding strict null checking

--- source.sh
+++ source.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )";
 PATH=$DIR/node_modules/.bin/:$PATH;
-alias build="tsc --target ES5 --lib ES2015 --skipLibCheck --sourceMap index.ts";
+alias build="tsc --target ES5 --lib ES2015 --skipLibCheck --sourceMap --strictNullChecks index.ts";
 alias start="webpack-dev-server & build; electron index.js | grep -v ERROR:CONSOLE &";
