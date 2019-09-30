daf38ea7dd128d3bcde9136ab7ccdeb3af073000
lschpmn <luis@lmc-productions.com>
2019-09-29 19:52:50 -0500

    Adding path to addTorrent function

--- server/torrent-service.ts
+++ server/torrent-service.ts
@@ -4,8 +4,8 @@ const client = new WebTorrent();
 
 export const Torrents: { [index: string]: WebTorrent.Torrent } = {};
 
-export function addTorrent(magnetId: string) {
-  client.add(magnetId, torrent => {
+export function addTorrent(magnetId: string, path: string) {
+  client.add(magnetId, { path }, torrent => {
     // bug workaround, https://github.com/webtorrent/webtorrent/issues/164#issuecomment-248395202
     torrent.deselect(0, torrent.pieces.length - 1, 0);
 
