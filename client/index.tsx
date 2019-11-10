4b329262615044b76594faa0e94a150e2886fc3d
lschpmn <luis@lmc-productions.com>
2019-11-10 02:09:41 -0600

    Removed unneeded packages

--- client/index.tsx
+++ client/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
 import App from './App';
 import electronMiddleware from './lib/electronMiddleware';
 import loggerMiddleware from './lib/loggerMiddleware';
@@ -11,7 +10,7 @@ import socketMiddleware from './lib/socketMiddleware';
 
 import './index.html';
 
-const store = createStore(reducers, applyMiddleware(thunk, loggerMiddleware, socketMiddleware, electronMiddleware));
+const store = createStore(reducers, applyMiddleware(loggerMiddleware, socketMiddleware, electronMiddleware));
 
 render(<Provider store={store}>
   <App />
