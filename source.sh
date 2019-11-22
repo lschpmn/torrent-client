46cb811789aae7aa3192b9dacf70e9d39b52b94a
lschpmn <luis@lmc-productions.com>
2019-11-22 00:22:01 -0600

    Updated source.sh because updated global nodemon version

--- source.sh
+++ source.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )";
 BIN=$DIR/node_modules/.bin
 PATH=$BIN/:$PATH;
-alias start-server="nodemon -e ts --watch ./server -x ./node_modules/.bin/ts-node ./server </dev/null"
+alias start-server="nodemon ./server/index.ts -w ./server/ </dev/null"
 alias start-client="electron index.js | grep -v ERROR:CONSOLE";
 alias start="start-server & start-client &";
