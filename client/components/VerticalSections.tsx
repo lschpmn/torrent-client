b8653d6c4064cbbc0f921a19272581b61c96d193
lschpmn <luis@lmc-productions.com>
2019-11-23 13:44:17 -0600

    Able to resize containers in VerticalSections

--- client/components/VerticalSections.tsx
+++ client/components/VerticalSections.tsx
@@ -1,7 +1,7 @@
 import Divider from '@material-ui/core/Divider';
 import throttle from 'lodash/throttle';
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 type Props = {
   child1: React.ReactNode,
@@ -10,15 +10,36 @@ type Props = {
 
 const VerticalSections = ({ child1, child2 }: Props) => {
   const [mouseY, setTrackingY] = useMouseYState();
-  console.log(mouseY);
+  const [elementBox, setElementBox]: [ClientRect, any] = useState(null);
+  const [node, setNode] = useState(null);
 
-  return <div style={styles.container}>
-    <div style={styles.section}>{child1}</div>
+  const percent = useMemo(() => {
+    if (mouseY && elementBox) {
+      let _percent = ((mouseY - elementBox.top) / elementBox.height * 100);
+      _percent = Math.max(Math.min(_percent, 99), 1);
+      return Math.round(_percent * 1000) / 1000;
+    } else return 50;
+  }, [mouseY, elementBox]);
+
+  console.log(percent);
+
+  useEffect(() => {
+    if (node) {
+      const getSizes = throttle(() => setElementBox(node.getBoundingClientRect()), 100);
+
+      getSizes();
+      window.addEventListener('resize', getSizes);
+      return () => window.removeEventListener('resize', getSizes);
+    }
+  }, [node]);
+
+  return <div style={styles.container} ref={setNode}>
+    <div style={{ ...styles.section, flex: percent }}>{child1}</div>
     <Divider
       onMouseDown={() => setTrackingY(true)}
-      style={{ cursor: 'ns-resize', height: '0.25rem' }}
+      style={{ cursor: 'ns-resize', height: '0.5rem' }}
     />
-    <div style={styles.section}>{child2}</div>
+    <div style={{ ...styles.section, flex: 100 - percent }}>{child2}</div>
   </div>;
 };
 
@@ -29,7 +50,7 @@ const useMouseYState = () => {
   useEffect(() => {
     if (isTracking) {
       const trackMouseUp = () => setIsTracking(false);
-      const trackMouseMovement = throttle(e => setMouseY(e.clientY), 100);
+      const trackMouseMovement = throttle(e => setMouseY(e.clientY), 15);
 
       document.addEventListener('mouseup', trackMouseUp);
       document.addEventListener('mousemove', trackMouseMovement);
