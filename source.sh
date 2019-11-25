2cc96a9879054c84e4b428677cd7dca52998e047
lschpmn <luis@lmc-productions.com>
2019-11-25 01:17:16 -0600

    Fix but in source.sh

--- source.sh
+++ source.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )";
 BIN=$DIR/node_modules/.bin
-PATH=$BIN/:$PATH;
+PATH=$PATH:$BIN;
 alias start-server="nodemon ./server/index.ts -w ./server/ </dev/null"
 alias start-client="electron index.js | grep -v ERROR:CONSOLE";
 alias start="start-server & start-client &";
